36fe9acd5f9701a6c69295c2b52cd038689725c4
MiguelC29 <miguelito28c@gmail.com>
2022-09-03 19:50:15 -0500

    agregando base de datos

---
+++ rockola06.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-09-2022 a las 02:19:28
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rockola06`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autor`
+--
+
+CREATE TABLE `autor` (
+  `idAutor` int(10) UNSIGNED NOT NULL,
+  `nombreAutor` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellidoAutor` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cancion`
+--
+
+CREATE TABLE `cancion` (
+  `idCancion` int(10) UNSIGNED NOT NULL,
+  `nombreCancion` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `idAutor` int(10) UNSIGNED NOT NULL,
+  `idGenero` int(10) UNSIGNED NOT NULL,
+  `fechaPublicacion` varchar(14) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generocancion`
+--
+
+CREATE TABLE `generocancion` (
+  `idGenero` int(10) UNSIGNED NOT NULL,
+  `genero` varchar(45) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`idAutor`);
+
+--
+-- Indices de la tabla `cancion`
+--
+ALTER TABLE `cancion`
+  ADD PRIMARY KEY (`idCancion`),
+  ADD KEY `idAutor` (`idAutor`,`idGenero`),
+  ADD KEY `idGenero` (`idGenero`);
+
+--
+-- Indices de la tabla `generocancion`
+--
+ALTER TABLE `generocancion`
+  ADD PRIMARY KEY (`idGenero`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `idAutor` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cancion`
+--
+ALTER TABLE `cancion`
+  MODIFY `idCancion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `generocancion`
+--
+ALTER TABLE `generocancion`
+  MODIFY `idGenero` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cancion`
+--
+ALTER TABLE `cancion`
+  ADD CONSTRAINT `cancion_ibfk_1` FOREIGN KEY (`idGenero`) REFERENCES `generocancion` (`idGenero`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cancion_ibfk_2` FOREIGN KEY (`idAutor`) REFERENCES `autor` (`idAutor`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
