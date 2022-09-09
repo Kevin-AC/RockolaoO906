4b355685c7720d07517f619736590ea16bfe45b9
kevin.argumedo <kevin.argumedo@gmail.com>
2022-09-09 17:46:43 -0500

    Archivos de Models

---
+++ RockolaO906/src/main/java/com/rockola06/RockolaO906/models/Cancion.java
@@ -0,0 +1,87 @@
+package com.rockola06.RockolaO906.models;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "cancion")
+public class Cancion implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCancion")
+    private Integer idCancion;
+    @Column(name = "nombreCancion")
+    private Integer nombreCancion;
+    @ManyToOne
+    @JoinColumn(name = "idAutor") // foarenea
+    private Autor autor;
+    @JoinColumn(name = "idGenero") // foraena}
+    private Genero genero;
+    @Column(name = "fechaPublicacion")
+    private Integer fechaPublicacion;
+
+    public Integer getIdCancion() {
+        return idCancion;
+    }
+    
+
+    public Cancion() {
+    }
+
+
+    public Cancion(Integer idCancion, Integer nombreCancion, Autor autor, Genero genero, Integer fechaPublicacion) {
+        this.idCancion = idCancion;
+        this.nombreCancion = nombreCancion;
+        this.autor = autor;
+        this.genero = genero;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+
+    public void setIdCancion(Integer idCancion) {
+        this.idCancion = idCancion;
+    }
+
+    public Integer getNombreCancion() {
+        return nombreCancion;
+    }
+
+    public void setNombreCancion(Integer nombreCancion) {
+        this.nombreCancion = nombreCancion;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public Integer getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(Integer fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+        
+  
+
+}
---
+++ RockolaO906/src/main/java/com/rockola06/RockolaO906/models/Genero.java
@@ -0,0 +1,48 @@
+package com.rockola06.RockolaO906.models;
+
+import java.io.Serializable;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "genero")
+public class Genero implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idGenero")
+    private Integer idGenero;
+    @Column(name = "genero")
+    private String genero;
+
+    public Genero() {
+    }
+
+    public Genero(Integer idGenero, String genero) {
+        this.idGenero = idGenero;
+        this.genero = genero;
+    }
+
+    public Integer getIdGenero() {
+        return idGenero;
+    }
+    public void setIdGenero(Integer idGenero) {
+        this.idGenero = idGenero;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+   
+    
+
+}
