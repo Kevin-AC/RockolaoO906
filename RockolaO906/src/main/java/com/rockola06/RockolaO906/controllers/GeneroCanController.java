649d3aa3a3125bcdc5b5c724e1bcf6046a3ea91e
MiguelC29 <miguelito28c@gmail.com>
2022-09-09 18:05:38 -0500

    creación de clases AutorController y GeneroCanController

---
+++ RockolaO906/src/main/java/com/rockola06/RockolaO906/controllers/GeneroCanController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rockola06.RockolaO906.controllers;
+
+/**
+ *
+ * @author migue
+ */
+public class GeneroCanController {
+    
+}
