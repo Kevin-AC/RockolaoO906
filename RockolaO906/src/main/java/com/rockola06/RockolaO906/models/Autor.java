5f46306506613ed7c5efd831dd00075533b59a5f
kevin.argumedo <kevin.argumedo@gmail.com>
2022-09-09 17:17:04 -0500

    Autor

--- RockolaO906/src/main/java/com/rockola06/RockolaO906/models/Autor.java
+++ RockolaO906/src/main/java/com/rockola06/RockolaO906/models/Autor.java
@@ -1,5 +1,43 @@
 package com.rockola06.RockolaO906.models;
 
-public class Autor {
-    
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "autor")
+public class Autor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idAutor")
+    private Integer idAutor;
+    @Column(name = "nombreAutor")
+    private String nombreAutor;
+    @Column(name = "apellidoAutor")
+    private String apellidoAutor;
+
+    public Integer getIdAutor() {
+        return idAutor;
+    }
+    public void setIdAutor(Integer idAutor) {
+        this.idAutor = idAutor;
+    }
+    public String getNombreAutor() {
+        return nombreAutor;
+    }
+    public void setNombreAutor(String nombreAutor) {
+        this.nombreAutor = nombreAutor;
+    }
+    public String getApellidoAutor() {
+        return apellidoAutor;
+    }
+    public void setApellidoAutor(String apellidoAutor) {
+        this.apellidoAutor = apellidoAutor;
+    }
+
 }
