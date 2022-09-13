168fbfa71cac7dc7db611a21abb1e1f46d2626b4
Dailet22 <dayanamaria5544@gmail.com>
2022-09-12 22:13:14 -0500

    configurando archivo properties y añadiendo archivos DAO al paquete repositories

---
+++ RockolaO906/src/main/java/com/rockola06/RockolaO906/services/repositories/AutorDAO.java
@@ -0,0 +1,8 @@
+package com.rockola06.RockolaO906.services.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.rockola06.RockolaO906.models.Autor;
+
+public interface AutorDAO extends CrudRepository <Autor, Integer>{
+    
+}
---
+++ RockolaO906/src/main/java/com/rockola06/RockolaO906/services/repositories/GeneroCanDAO.java
@@ -0,0 +1,9 @@
+package com.rockola06.RockolaO906.services.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.rockola06.RockolaO906.models.Genero;
+
+public interface GeneroCanDAO extends CrudRepository <Genero, Integer> {
+
+   
+}
